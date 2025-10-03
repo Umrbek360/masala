3eefd82bbf2edfc714f0eb9de1fb5669fc2b7a42
umrbek <umrbekobdullayev@gmail.com>
2025-10-03 15:33:04 +0500

    Add files via upload

---
+++ masala.py
@@ -0,0 +1,5 @@
+a = input("Familyanggizni kiriting: ")
+b = input("ismingizni kiriting: ")
+c = input("otangizni ismini kiriting: ")
+
+print(a.capitalize(), b.capitalize(), c.capitalize(),("o'g'li"))
